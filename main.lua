df6921851b81bf704658c30be9a508c1f41eb02e
tma-11080 <rvou29+1@gmail.com>
2026-03-25 17:34:18 +0900

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,14 @@
+local base = "https://raw.githubusercontent.com/USER/REPO/main/"
+
+loadstring(game:HttpGet(base.."config.lua"))()
+
+loadstring(game:HttpGet(base.."core/ui.lua"))()
+loadstring(game:HttpGet(base.."core/font.lua"))()
+
+loadstring(game:HttpGet(base.."tabs/mac.lua"))()
+loadstring(game:HttpGet(base.."tabs/mainfarm.lua"))()
+loadstring(game:HttpGet(base.."tabs/misc.lua"))()
+
+loadstring(game:HttpGet(base.."modules/esp.lua"))()
+loadstring(game:HttpGet(base.."modules/combat.lua"))()
+loadstring(game:HttpGet(base.."modules/movement.lua"))()
