78e0a3393c464fa06f8e6faea63a95d0a10db908
tma-11080 <rvou29+1@gmail.com>
2026-03-25 17:37:23 +0900

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+getgenv().mac_config = {
+
+    auraRange = 50,
+    attackSpeed = 0.1,
+    moveSpeed = 0.15,
+    height = 5,
+
+    playerESP = false,
+    chestESP = false,
+    followPlayer = false,
+    followEnemy = false
+
+}
