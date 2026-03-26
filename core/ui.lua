d8c15a43770cb9ff86dd4697fd14ac3aaef6d5b5
tma-11080 <rvou29+1@gmail.com>
2026-03-26 16:53:16 +0900

    Delete core directory

--- core/ui.lua
+++
@@ -1,9 +0,0 @@
-local Library = loadstring(game:HttpGet(
-"https://raw.githubusercontent.com/tma-11080/NeonGlass-UI.v2/refs/heads/main/main_redzglass.luau"
-))()
-
-Window = Library:MakeWindow({
- Title = "mac lol Hub : Cool Game",
- SubTitle = "by real_redz",
- ScriptFolder = "redz-library-V5"
-})
